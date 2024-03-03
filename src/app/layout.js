bf33bb685a3f7266ce59139eba4c1b13a45a02a1
Olumide <96479799+olumidedaramola21@users.noreply.github.com>
2024-03-04 00:27:26 +0100

    added components directory and added new lines in layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/navbar/Navbar";
+import Footer from "@/components/footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,9 +15,9 @@ export default function RootLayout({ children }) {
     <html lang="en">
     
       <body className={inter.className}>
-      
+      <Navbar />
         {children}
-        
+       <Footer /> 
         
       </body>
       
