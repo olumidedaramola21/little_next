846ea753b34747c18c88ed3c06af22bc58b58df1
Olumide <96479799+olumidedaramola21@users.noreply.github.com>
2024-03-11 20:48:33 +0100

    linking external images in next

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,6 +1,12 @@
+import Image from 'next/image'
+import styles from './about.module.css'
+
 const AboutPage = () => {
     return (
-       <img src="/about.png" alt="about"/>
+    <div className={styles.imgContainer}>
+        <Image src="https://images.pexels.com/photos/20165148/pexels-photo-20165148/free-photo-of-kaho.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt='' fill/>
+    </div>
+    
     )
 }
 
